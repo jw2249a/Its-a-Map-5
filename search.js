38fe675f84e44bf6e17cb544d7ccf7c3fac3bf34
jw2249a <jw2249a@student.american.edu>
2017-05-11 13:39:18 -0400

    Its-a-Map-5

--- search.js
+++ search.js
@@ -10,21 +10,6 @@ var searchControl = new L.Control.Search({
       map.setView(latlng, zoom-1); // access the zoom
   }
 });
-function onEachFeature(feature, layer){
-  searchControl.on('search:locationfound', function(e) {
-    var popup = L.popup({closeButton:false})
-    .setLatLng(e.latlng)
-          .setContent("<table>" +
-          "<tr class='oddrowcol'><td><b>Name: </b></td><td>" + feature.properties["NAME"] + "</td></tr>" +
-          "</table>")
-          .openOn(map);
-  });
-  layer.on('mouseover', function (e) {
-	this.closePopup();
-});
-popup.on('mouseover', function (e) {
-	this.closePopup();
-});
 
 
 map.addControl(searchControl);
