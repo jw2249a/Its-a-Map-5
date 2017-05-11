c0ab04ef46f7192cd6042476de6c803e556d607c
Jack Williams <jw2249a@users.noreply.github.com>
2017-05-11 13:49:51 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -10,25 +10,6 @@ var searchControl = new L.Control.Search({
       map.setView(latlng, zoom-1); // access the zoom
   }
 });
-<<<<<<< HEAD
-
-=======
-function onEachFeature(feature, layer){
-  searchControl.on('search:locationfound', function(e) {
-    var popup = L.popup({closeButton:false})
-    .setLatLng(e.latlng)
-          .setContent("<table>" +
-          "<tr class='oddrowcol'><td><b>Name: </b></td><td>" + feature.properties["NAME"] + "</td></tr>" +
-          "</table>")
-          .openOn(map);
-  });
-  layer.on('mouseover', function (e) {
-	this.closePopup();
-});
-popup.on('mouseover', function (e) {
-	this.closePopup();
-});
->>>>>>> a9570d3bbb6b735f540a8ab986a56bdc15b7cf99
 
 
 map.addControl(searchControl);
